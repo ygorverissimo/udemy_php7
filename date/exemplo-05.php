f383b908be7478cd2177dc95daf0d69065595f39
Ygor Veríssimo <ygor.verissimo@gmail.com>
2024-07-22 18:38:29 -0300

    Update exemplo-05.php

--- date/exemplo-05.php
+++ date/exemplo-05.php
@@ -2,14 +2,14 @@
 
 $dt = new DateTime();
 
-//atribui a periodo um intervalo de tempo de 15 dias ao objeto DateInterval, que é atribuído à variável $periodo
+//atribui à variável $periodo um intervalo de tempo de 15 dias usando o objeto DateInterval, que é usado para guardar um intervalo de tempo
 $periodo = new DateInterval("P15D");
 
 echo $dt->format("d/m/Y H:i:s");
 echo "<br>";
 
 //o método add, da classe DateTime, adiciona um intervalo de tempo à data
-//o método add recebe um objeto DateInterval como parâmetro, que guarda um intervalo de tempo
+//o método add recebe um objeto DateInterval como parâmetro
 //como esse intervalo de tempo já foi atribuído à variável $periodo, podemos apenas chamá-la
 $dt->add($periodo);
 //podemos atribuir o DateInterval diretamente no parâmetro do método add se quisermos também
